17bcc4acab52171b3ac12406cd42027158dffb82
Andrey Weber <Andrey.Weber@gmail.com>
2016-11-16 13:04:49 +0100

    Slightly improved example of JS Spreadsheet API calls has been provided

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # fes-react-course-marksboard
-Marks board for Front-end Science ReactJS course marks board application
+Marks board application for Front-end Science ReactJS course.
 
 ## Useful Links
 1. [Google API Client Libraries > JavaScript (Beta)](https://developers.google.com/api-client-library/javascript/start/start-js)
-2. [Best practices for securely using API keys](https://support.google.com/cloud/answer/6310037)
-3. [Google Sheets API > JavaScript Quickstart](https://developers.google.com/sheets/quickstart/js)
-4. [Google Spreadsheet ID explanation](https://developers.google.com/sheets/guides/concepts#spreadsheet_id)
-5. [Google Sheets API > Samples > Basic Reading](https://developers.google.com/sheets/samples/reading)
-6. [Google Developers Console](https://console.developers.google.com)
+2. [Google API Client Libraries > JavaScript (Beta) > Methods and Classes](https://developers.google.com/api-client-library/javascript/features/batch)
+3. [Best practices for securely using API keys](https://support.google.com/cloud/answer/6310037)
+4. [Google Sheets API > JavaScript Quickstart](https://developers.google.com/sheets/quickstart/js)
+5. [Google Spreadsheet ID explanation](https://developers.google.com/sheets/guides/concepts#spreadsheet_id)
+6. [Google Sheets API > Samples > Basic Reading](https://developers.google.com/sheets/samples/reading)
+7. [Google Developers Console](https://console.developers.google.com)
--- src/main.js
+++ src/main.js
@@ -1,21 +1,27 @@
 function start() {
     // 2. Initialize the JavaScript client library.
     gapi.client.init({
-        'apiKey': '<API_KEY>'
+        'apiKey': '<API_KEY>',
         // clientId and scope are optional if auth is not required.
         //'clientId': 'YOUR_WEB_CLIENT_ID.apps.googleusercontent.com',
-        //'scope': 'profile',
+        //'scope': 'https://www.googleapis.com/auth/spreadsheets.readonly',
+        //'immediate': true
     }).then(function() {
-        // 3. Initialize and make the API request.
-        return gapi.client.request({
-            'path': 'https://sheets.googleapis.com/v4/spreadsheets/<SPREADSHEET_ID>/values/Sheet1!A2:M3',
-        })
-    }).then(function(resp) {
-        console.log(resp.result);
-    }, function(reason) {
-        console.log('Error: ' + reason.result.error.message);
+        const discoveryUrl = 'https://sheets.googleapis.com/$discovery/rest?version=v4';
+        gapi.client.load(discoveryUrl).then(function() {
+            gapi.client.sheets.spreadsheets.values.get({
+                spreadsheetId: '<SPREADSHEET_ID>',
+                majorDimension: 'COLUMNS',  // ROWS
+                range: 'Sheet1!A2:A',       // Sheet1!A2:M2
+            }).then(function(response) {
+                console.log(JSON.stringify(response.body));
+                console.log(response);
+            }, function(response) {
+                console.log('Error: ' + response.result.error.message);
+            });
+        });
     });
-};
+}
 
 // 1. Load the JavaScript client library.
 gapi.load('client', start);
