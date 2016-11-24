77af0bce6342e87d9c387837cb1968caaf60b214
Andrey Weber <Andrey.Weber@gmail.com>
2016-11-24 09:06:36 +0100

    Real API Key and Spreadsheet Id values removed from configuration file.

--- public/static/config.js
+++ public/static/config.js
@@ -1,5 +1,5 @@
 var APP_CONFIG = {
-    apiKey:         'AIzaSyDu8kDrHdk0QEFCYe0LZn4rm7phXZ9tJ7g',
-    spreadsheetId:  '1C7PWqTPcK1IbDWTbYH_NkDQOszKD5dtVW51i9SW-UxU',
+    apiKey:         '<API_KEY>',
+    spreadsheetId:  '<SPREADSHEET_ID>',
     discoveryUrl:   'https://sheets.googleapis.com/$discovery/rest?version=v4'
 };
